32d54c3befa544cf2c467506c231a6c1563539cd
dtHvinh <dangthehienvinh662@gmail.com>
2026-08-08 15:14:25 +0700

    refactore folder structure

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,7 @@ rootProject.name = "link-forge"
 include("app")
 include("domain")
 include("utils")
+
+project(":app").projectDir = file("src/app")
+project(":domain").projectDir = file("src/domain")
+project(":utils").projectDir = file("src/utils")
